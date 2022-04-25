8ff3643007786899b37b5a4ab3711b567c26fbc3
Sbbah Eltag Elnour <95896938+sbbahh@users.noreply.github.com>
2022-04-25 08:41:35 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-fetch('http://api.worldbank.org/v2/country/')
-    .then(function (res) {
-        console.log(res);
-        // return JSON.parse(res);
-    }).then(function (data) {
-        console.log(data.adminregion);
-    }).catch(function (error) {
-        console.log(error);
-    })
